cde087b91812202e7d357788df6f95250ac0fd32
AMSAVARTHAN-k <kenndyamsavarthan45@gmail.com>
2023-11-14 21:03:37 +0530

    divthree finished

---
+++ School/assets/script.js
@@ -0,0 +1,10 @@
+let isExpanded = false;
+function toggleHeight() {
+  const bgs = document.getElementById("bgs");
+  if (isExpanded) {
+    bgs.style.height = "822px";
+  } else {
+    bgs.style.height = "1000px";
+  }
+  isExpanded = !isExpanded;
+}
